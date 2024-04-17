ad5ee5d2a2b7ec7ce07b5654fbcbcc196cbcc2b2
AleksandraCherevko <119119431+AleksandraCherevko@users.noreply.github.com>
2024-04-17 13:26:44 +0200

    dw

---
+++ js/lightbox.js
@@ -0,0 +1,3 @@
+const listEl = document.querySelector('.ad-criteria-list');
+const itemEl = document.querySelector('.ad-criteria-item');
+const pictureEl = document.querySelector('.criteria-image');
